4a2b3a4e05f3711634da1f890b0e136ee4ddd9e8
Frida 3 <149642064+frida-al@users.noreply.github.com>
2024-05-11 19:47:48 -0600

    Update Input.java

--- src/Input.java
+++ src/Input.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Input {
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Input {
+    public static int inputNumber(String inputMessage, String errorMessage,int option) {
+        Scanner input = new Scanner(System.in);
+        int valueInput;
+        while (true) {
+            System.out.println(inputMessage);
+            try {
+                valueInput = input.nextInt();
+                if (valueInput > option && option != 0) {
+                    throw new InputMismatchException();
+                }
+                input.nextLine();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println(errorMessage);
+                input.nextLine();
+            }
+        }
+        return valueInput;
+    }
 }
